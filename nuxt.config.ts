61d3297df8b577621d8876f0e42811fc64dba978
bankk <suriya.yim1150@gmail.com>
2025-03-18 19:56:01 +0700

    install tauri

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,6 +3,22 @@ export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
 
+  ssr: false,
+  devServer: { host: process.env.TAURI_DEV_HOST || 'localhost' },
+
+  vite: {
+    // Better support for Tauri CLI output
+    clearScreen: false,
+    // Enable environment variables
+    // Additional environment variables can be found at
+    // https://v2.tauri.app/reference/environment-variables/
+    envPrefix: ['VITE_', 'TAURI_'],
+    server: {
+      // Tauri requires a consistent port
+      strictPort: true,
+    },
+  },
+
   modules: [
     '@nuxt/eslint',
     '@nuxt/fonts',
